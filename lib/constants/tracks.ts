2810243034f6e9461a298532a23399279e0c4c60
Loadstring-Afk <elvismateopedro@gmail.com>
2025-12-19 17:13:44 -0600

    Create lib/constants/tracks.ts

---
+++ lib/constants/tracks.ts
@@ -0,0 +1,28 @@
+import { Track } from '@/types';
+
+export const tracks: Track[] = [
+  {
+    id: '1',
+    title: 'Ambient Dreams',
+    album: 'Cinematic Atmospheres',
+    artist: 'Alex Chen',
+    src: '/music/track1.mp3',
+    duration: 240,
+  },
+  {
+    id: '2',
+    title: 'Digital Waves',
+    album: 'Electronic Journeys',
+    artist: 'Alex Chen',
+    src: '/music/track2.mp3',
+    duration: 210,
+  },
+  {
+    id: '3',
+    title: 'Future Memories',
+    album: 'Synth Explorations',
+    artist: 'Alex Chen',
+    src: '/music/track3.mp3',
+    duration: 195,
+  },
+];
